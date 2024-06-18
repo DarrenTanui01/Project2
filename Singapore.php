f627205f4793e58078b5ff80c3bcacfdfc664fb7
DarrenTanui01 <darren.tanui@strathmore.edu>
2024-06-18 05:24:58 +0300

    converted singapore.html to singapore.php

---
+++ Singapore.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/singapore.css">
+</head>
+<style>
+    body{
+        background-color: darksalmon;
+    }
+</style>
+<body>
+    <?php
+    
+    echo '<header>';
+    echo '<h1 align = "center" style="color: antiquewhite;" >Singapore</h1>';
+    echo '<nav>';
+    echo '<ul >';
+    echo '<a href="Home.html"  style="color: antiquewhite;">HOME</a>';
+    echo '<a href="BOOKINGS.html"  style="color: antiquewhite;">BOOKINGS</a>';
+    echo '<a href="ABOUT US.html"  style="color: antiquewhite;">ABOUT US</a>';
+    echo '<a href="CONTACTS.html"  style="color: antiquewhite;">CONTACTS</a>';
+    echo '</ul>';
+    echo '</nav>';
+    echo '</header>';
+    
+    echo '<p>Nestled in Southeast Asia, the vibrant city-state of Singapore beckons tourists with its mesmerizing blend of modernity and cultural heritage. Renowned as a global financial hub, this island nation also boasts a rich tapestry of diverse communities, culinary delights, and awe-inspiring attractions. Some key take-aways about the city:';
+    echo '<li>Thriving island nation: A small, Southeast Asian country known for its economic success and modern cityscape.</li>';
+    echo '<li>Cultural melting pot: A blend of Chinese, Malay, Indian, and European influences reflected in its cuisine, architecture, and traditions.</li>';
+    echo '<li>Garden city: A commitment to green spaces with parks and gardens integrated throughout the urban environment.</li>';
+    echo '<li> Strict and efficient: Known for its clean streets, low crime rates, and emphasis on orderliness.</li>';
+    echo '</p>';
+    
+    echo '<img src = "images/Singapore/Singapore1.jpg" alt = "" width = "45%">';
+    echo '<img src = "images/Singapore/Singapore2.jpg" alt = "" width = "50%">';
+    echo '<img src = "images/Singapore/Singapore3.jpg" alt = "" width = "100%"> ';
+    
+    echo '<footer>© 2024 TravelNestor. All Rights Reserved.</footer>';   
+   ?>
+</body>
+</html>
