c41a752c12bcbe50c8737b82032e5d79a6bcd8a4
DarrenTanui01 <darren.tanui@strathmore.edu>
2024-06-18 05:19:12 +0300

    converted home.html to home.php

---
+++ Home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <?php
+    
+    echo '<header>';
+    echo '<img src="images/Singapore/Singapore5.png" alt="Icon" class="icon-placeholder">';
+    echo '<h1>TravelNestor-Singapore</h1>';
+    echo '<nav>';
+    echo '<ul style="text-align: right;">';
+    echo '<a href="BOOKINGS.html">BOOKINGS</a>';
+    echo '<a href="ABOUT US.html">ABOUT US</a>';
+    echo '<a href="CONTACTS.html">CONTACTS</a>';
+    echo '</ul>';
+    echo '</nav>';
+    echo '</header>'; 
+    echo '<main>';
+    echo '<section id="Singapore">';
+    echo '<h2>Singapore in a nutshell!</h2>';
+    echo '<a href="Singapore.html">LEARN MORE</a>';
+    echo '</section>';
+
+    echo '<section id="Marina Bay Sands">';
+    echo '<h2>Redefining the city skyline...</h2>';
+    echo '<a href="Marina Bay Sands.html">LEARN MORE</a>';
+    echo '</section>';
+
+    echo '<section id="Gardens by the Bay">';
+    echo '<h2>Testament to Singapore\'s sustainability...</h2>';
+    echo '<a href="Gardens By the Bay.html">LEARN MORE</a>';
+    echo '</section>';
+
+    echo '<section id="Chinatown">';
+    echo '<h2>Singapore\'s Chinese heritage...</h2>';
+    echo '<a href="ChinaTown.html">LEARN MORE</a>';
+    echo '</section>';
+    echo '</main>';
+    echo '<footer>';
+    echo '<p style="text-align: center;">©  2024 TravelNestor. All Rights Reserved.</p>';
+    echo '</footer>';   
+   ?>
+</body> 
+</html>
