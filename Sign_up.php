23df2a30453b2128f80e5354e1190a9ac0ab2785
DarrenTanui01 <darren.tanui@strathmore.edu>
2024-07-11 16:42:56 +0300

    moved signup.css to css folder, and changed the href

--- Sign_up.php
+++ Sign_up.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TravelNestor Sign Up</title>
-  <link rel="stylesheet" href="sign_up.css">
+  <link rel="stylesheet" href="CSS/sign_up.css">
 </head>
 <body>
   <header>
